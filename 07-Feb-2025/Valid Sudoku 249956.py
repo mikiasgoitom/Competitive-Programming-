399d1c21fea5f2d9d7e4e232f2ffa054defb61b0
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-02-07 16:32:11 +0300

    Created 'Valid Sudoku' by A2SV Hub

---
+++ 07-Feb-2025/Valid Sudoku 249956.py
@@ -0,0 +1,26 @@
+# Problem: Valid Sudoku - https://leetcode.com/problems/valid-sudoku/
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        dic_row = defaultdict(list)
+        dic_col = defaultdict(list)
+        dic_con = defaultdict(list)
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] != '.':
+                    if board[row][col] in dic_row[row]:
+                        return False
+                    else:
+                        dic_row[row].append(board[row][col])
+                    if board[row][col] in dic_col[col]:
+                        print("col", col)
+                        return False
+                    else:
+                        dic_col[col].append(board[row][col])
+                    if board[row][col] in dic_con[tuple([row//3, col//3])]:
+                        print("con", tuple([row//3, col//3]))
+                        return False
+                    else:
+                        dic_con[tuple([row//3, col//3])].append(board[row][col])
+                    
+        return True
