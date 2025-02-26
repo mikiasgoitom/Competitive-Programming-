ab7dc1e8af302ae6dab48db12f98d75d16089321
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-02-26 17:09:29 +0300

    Created 'Linked List Cycle II' by A2SV Hub

---
+++ 26-Feb-2025/Linked List Cycle II 283835.py
@@ -0,0 +1,29 @@
+# Problem: Linked List Cycle II - https://leetcode.com/problems/linked-list-cycle-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        met = False
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                met = True
+                break
+        if not met:
+            return None
+        else:
+            slow = head
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            return slow
+        return None
