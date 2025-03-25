5e9f5f5167aef39b50e7e45e86305c922033d3ea
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-03-25 16:52:19 +0300

    Created 'Sqrt(x)' by A2SV Hub

---
+++ 25-Mar-2025/Sqrt(x) 317630.py
@@ -0,0 +1,15 @@
+# Problem: Sqrt(x) - https://leetcode.com/problems/sqrtx/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x
+
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == x:
+                return mid
+            if mid * mid > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
