9c3bae97685bf8bbe00bacc250624dd8368c9cab
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-04-13 09:52:01 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 13-Apr-2025/Combinations 332813.py
@@ -0,0 +1,8 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(combinations(range(1, n+1), k))
+        
+
+
