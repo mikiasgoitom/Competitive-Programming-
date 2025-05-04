265ca91257c50072758d0d5de07e66f35b48e823
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-05-04 15:29:50 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 04-May-2025/Thearte Square 348024.py
@@ -0,0 +1,24 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import sys
+from math import ceil, gcd, log2
+from collections import defaultdict, Counter
+
+def in_str() -> str: return sys.stdin.readline().strip()
+def in_int() -> int: return int(sys.stdin.readline().strip())
+def in_integers(): return map(int, sys.stdin.readline().strip().split())
+def in_list() -> list: return list(map(int, sys.stdin.readline().strip().split()))
+def in_strings(): return sys.stdin.readline().strip().split()
+def in_string_list() -> list: return list(sys.stdin.readline().strip())
+def in_matrix(n: int) -> list: return [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+
+def solve():
+    n, m, a = in_integers()
+    ans = ceil(n/a) * ceil(m/a)
+    print(ans)
+
+def main():
+    solve()
+
+if __name__ == "__main__":
+    main()
