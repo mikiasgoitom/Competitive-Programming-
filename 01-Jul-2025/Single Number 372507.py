d608f5d1be3bd8077a7a4dc2995ce70c0e9f6e2f
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-07-01 21:40:24 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 01-Jul-2025/Single Number 372507.py
@@ -0,0 +1,8 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num = nums[0]
+        for i in range(1, len(nums)):
+            num = num ^ nums[i]
+        return num
