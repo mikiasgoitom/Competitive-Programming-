634f6ec0ffc80afe5883a0aa28855575b0f6afe3
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-07-04 23:24:46 +0300

    Created 'Maximal Square' by A2SV Hub

---
+++ 04-Jul-2025/Maximal Square 373551.py
@@ -0,0 +1,25 @@
+# Problem: Maximal Square - https://leetcode.com/problems/maximal-square/
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        cache = {}
+
+        def helper(r, c):
+            if r >= rows or c >= cols:
+                return 0
+            
+            if (r, c) not in cache:
+                right = helper(r + 1, c)
+                left = helper(r, c + 1)
+                diag = helper(r+1, c+1)
+
+                cache[(r,c)] = 0
+
+                if matrix[r][c] == "1":
+                    cache[(r,c)] = 1 + min(right, left, diag)
+            return cache[(r,c)]
+        
+        helper(0, 0)
+
+        return max(cache.values()) ** 2
