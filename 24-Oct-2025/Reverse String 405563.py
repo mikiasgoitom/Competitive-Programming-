cf4efbcd52f600f17c767b3a74c6cea46fca5655
Mikias Goitom <mikiasgoitom7@gmail.com>
2025-10-24 19:23:35 +0300

    Created 'Reverse String' by A2SV Hub

---
+++ 24-Oct-2025/Reverse String 405563.py
@@ -0,0 +1,12 @@
+# Problem: Reverse String - https://leetcode.com/problems/reverse-string/description/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) -1 
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            r -= 1
+            l += 1     
